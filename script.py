f5ebbe052d2763db5960a5d653775de68cf5a933
Dominik <dommal13@st.amu.edu.pl>
2025-10-21 17:42:39 +0200

    working ocr + beginning of maps

--- script.py
+++ script.py
@@ -1,11 +1,29 @@
 import pytesseract
 import pyautogui
+from pynput import keyboard
 from PIL import Image
+import cv2
+def map_name_to_text():
+    screen_size_x, screen_size_y = pyautogui.size()
+    crop = int(screen_size_y * 0.7)
+    width = int(screen_size_x * 0.4) - 50
+    height = screen_size_y - crop - 60
+    print(height)
+    screenshot = pyautogui.screenshot('screenshot.png',region=(0, crop, width, height))
+    img = cv2.imread('screenshot.png')
+    mask = cv2.inRange(img, (250, 250, 250), (255, 255, 255))
+    text = pytesseract.image_to_string(mask)
+    cv2.imwrite("mask.png", mask)
+    print(text)
+def on_press(key):
+    try:
+        if key.char == 'p':
+            map_name_to_text()
+    except AttributeError:
+        pass
+
 pytesseract.pytesseract.tesseract_cmd = r"C:\Program Files\Tesseract-OCR\tesseract.exe"
-print(pytesseract.image_to_string(Image.open('test2.png')))
-screen_size_x, screen_size_y = pyautogui.size()
-print(type(screen_size_y))
-crop = int(screen_size_y * 0.7)
-width = int(screen_size_x * 0.4)
-height = screen_size_y - crop
-screenshot = pyautogui.screenshot('screenshot.png',region=(0, crop, width, height))
+listener = keyboard.Listener(on_press=on_press)
+listener.start()
+while True:
+    pass
